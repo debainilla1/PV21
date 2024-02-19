7befc42972ac06df51e77297e1e349a819f10288
debainilla12 <debanhiabigaillibradoalmada@hotmail.com>
2024-02-19 11:20:32 -0600

    Agregar archivos de proyecto.

---
+++ PV2/FrmStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PV2
+{
+    public partial class FrmStudent : Form
+    {
+        public FrmStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void BttPressMe_Click(object sender, EventArgs e)
+        {
+            // estructuras de control
+            if (this.TxtName.Text != string.Empty)
+            {
+                this.LblName.Text = this.TxtName.Text;
+            
+            }
+            
+        }
+    }
+}
+
