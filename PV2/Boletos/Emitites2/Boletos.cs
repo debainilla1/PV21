cb372ac5a10844e2484d2a106db4dbf284ca7ecc
debainilla1 <debanhiabigaillibradoalmada@hotmail.com>
2024-03-04 11:12:28 -0600

    1

---
+++ PV2/Boletos/Emitites2/Boletos.cs
@@ -0,0 +1,53 @@
+﻿namespace PV2.Boletos.Emitites2
+{
+    public class ScolarshipHolder
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public int NumberAssignment { get; set; }
+        public string Campus { get; set; }
+        public ScolarshipHolder()
+
+        { }
+    }
+
+    public class Buyer
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string ScolarshipHolderName { get; set; }
+        public Buyer()
+        { }
+
+    }
+    public class Seller
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int TicketNumbers { get; set; }
+        public Seller()
+        { }
+
+    }
+    public class Winners
+    {
+
+        public string PrizesList { get; set; }
+        public int WinningTickets { get; set; }
+        public Winners()
+        { }
+
+    }
+}
+
+   
